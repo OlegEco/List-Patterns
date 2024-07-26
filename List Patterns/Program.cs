63246f74addb5681824a64220ca8a2c56ba7563b
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-26 09:36:50 +0200

    Single vs Multiple

--- List Patterns/Program.cs
+++ List Patterns/Program.cs
@@ -1,8 +1,14 @@
-﻿object[] items = { "item1"};
+﻿List<int> numbers1 = new List<int> { 123 };
+List<int> numbers2 = new List<int> { 32, 59, 1384, 14 };
 
-if (items is [var item])
+if (numbers1 is [var singleItem])
 {
-    Console.WriteLine($"Single item found: {item}");
+    Console.WriteLine($"The single item is: {singleItem}");
 }
-else Console.WriteLine("Multiple or no items found");
+else Console.WriteLine("The list doesn't have exactly one item");
 
+if (numbers2 is [var singleItem2])
+{
+    Console.WriteLine($"The single item is: {singleItem2}");
+}
+else Console.WriteLine("The list doesn't have exactly one item");
